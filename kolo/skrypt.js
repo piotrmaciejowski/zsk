2dbc6e4c91c4667c0b0b4779a1e1da576998be52
piotrmaciejowski <maciejowski.piotrek@gmail.com>
2017-09-14 00:47:19 +0200

    .

--- kolo/skrypt.js
+++ kolo/skrypt.js
@@ -1,26 +1,30 @@
-function oblicz(promien, wysokosc){
+function oblicz(r, h){
 
+    //Definicja zmiennych z formularza
 	var r = document.getElementById('promien').value;
     var h = document.getElementById('wysokosc').value;
-
-    var Obw = 2 * Math.PI * r;
-    var Pol = Math.PI * Math.pow(r, 2);
-    var Obj = Pol * h / 3;
-
-    var wynik = [Obw, Pol, Obj];
 
     var elObwod = document.getElementById('obwod');
     var elPole = document.getElementById('pole');
     var elObjetosc = document.getElementById('objetosc');
 
+    //Działania
+    var Obw = 2 * Math.PI * r;
+    var Pol = Math.PI * Math.pow(r, 2);
+    var Obj = Pol * h / 3;
+
+    //Wynik
+    var wynik = [Obw, Pol, Obj];
+
+    //Wyświetlanie obliczeń
     if (elObwod.checked == true) {
-        document.getElementById('wyniki').innerHTML = Obw.toFixed(4) + ' cm2';
+        document.getElementById('wyniki').innerHTML = Obw.toFixed(4) + ' cm' + '2'.sup();
         document.getElementById('naglowek').innerHTML = 'Obwód:'
     } else if (elPole.checked == true) {
-            document.getElementById('wyniki').innerHTML = Pol.toFixed(4) + ' cm2';
+            document.getElementById('wyniki').innerHTML = Pol.toFixed(4) + ' cm' + '2'.sup();
             document.getElementById('naglowek').innerHTML = 'Pole:'
     } else if (elObjetosc.checked == true) {
-                document.getElementById('wyniki').innerHTML = (Obj * 0.001).toFixed(4) + ' dm3';
+                document.getElementById('wyniki').innerHTML = (Obj * 0.001).toFixed(4) + ' dm' + '3'.sup();
                 document.getElementById('naglowek').innerHTML = 'Objętość:'
             };
 
