6a68a4be9a92b9a8cac08c0865811ef1d910fbb7
piotrmaciejowski <maciejowski.piotrek@gmail.com>
2017-09-14 14:26:26 +0200

    Poprawki - dodanie zmiennych

--- kolo/skrypt.js
+++ kolo/skrypt.js
@@ -4,9 +4,10 @@ function oblicz(r, h){
 	var r = document.getElementById('promien').value;
     var h = document.getElementById('wysokosc').value;
 
-    var elObwod = document.getElementById('obwod');
-    var elPole = document.getElementById('pole');
-    var elObjetosc = document.getElementById('objetosc');
+    var elObwodRadio = document.getElementById('obwod');
+    var elPoleRadio = document.getElementById('pole');
+    var elObjetoscRadio = document.getElementById('objetosc');
+
 
     //Działania
     var Obw = 2 * Math.PI * r;
@@ -16,19 +17,22 @@ function oblicz(r, h){
     //Wynik
     var wynik = [Obw, Pol, Obj];
 
+	var elWyniki = document.getElementById('wyniki');
+	var elNaglowek = document.getElementById('naglowek');
+
     //Wyświetlanie obliczeń
-    if (elObwod.checked == true) {
-        document.getElementById('wyniki').innerHTML = Obw.toFixed(4) + ' cm' + '2'.sup();
-        document.getElementById('naglowek').innerHTML = 'Obwód:';
-    } else if (elPole.checked == true) {
-            document.getElementById('wyniki').innerHTML = Pol.toFixed(4) + ' cm' + '2'.sup();
-            document.getElementById('naglowek').innerHTML = 'Pole:';
-    } else if (elObjetosc.checked == true) {
-                document.getElementById('wyniki').innerHTML = (Obj * 0.001).toFixed(4) + ' dm' + '3'.sup();
-                document.getElementById('naglowek').innerHTML = 'Objętość:';
+    if (elObwodRadio.checked == true) {
+        elWyniki.innerHTML = Obw.toFixed(4) + ' cm' + '2'.sup();
+        elNaglowek.innerHTML = 'Obwód:';
+    } else if (elPoleRadio.checked == true) {
+            elWyniki.innerHTML = Pol.toFixed(4) + ' cm' + '2'.sup();
+            elNaglowek.innerHTML = 'Pole:';
+    } else if (elObjetoscRadio.checked == true) {
+                elWyniki.innerHTML = (Obj * 0.001).toFixed(4) + ' dm' + '3'.sup();
+                elNaglowek.innerHTML = 'Objętość:';
             } else {
-                document.getElementById('wyniki').innerHTML = 'Wybierz obliczenie i wprowadź dane!';
-                document.getElementById('naglowek').innerHTML = '';
+                elWyniki.innerHTML = 'Wybierz obliczenie i wprowadź dane!';
+                elNaglowek.innerHTML = '';
             };
 
     return wynik;
