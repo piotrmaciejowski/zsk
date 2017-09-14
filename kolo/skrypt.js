3ca9939da63624c664350b1d3e82c864b81c1cf9
piotrmaciejowski <maciejowski.piotrek@gmail.com>
2017-09-14 19:42:46 +0200

    Spacje

--- kolo/skrypt.js
+++ kolo/skrypt.js
@@ -21,14 +21,14 @@ function oblicz(r, h){
         document.getElementById('wyniki').innerHTML = Obw.toFixed(4) + ' cm' + '2'.sup();
         document.getElementById('naglowek').innerHTML = 'Obwód:';
     } else if (elPole.checked == true) {
-            document.getElementById('wyniki').innerHTML = Pol.toFixed(4) + ' cm' + '2'.sup();
-            document.getElementById('naglowek').innerHTML = 'Pole:';
+        document.getElementById('wyniki').innerHTML = Pol.toFixed(4) + ' cm' + '2'.sup();
+        document.getElementById('naglowek').innerHTML = 'Pole:';
     } else if (elObjetosc.checked == true) {
-            document.getElementById('wyniki').innerHTML = (Obj * 0.001).toFixed(4) + ' dm' + '3'.sup();
-            document.getElementById('naglowek').innerHTML = 'Objętość:';
+        document.getElementById('wyniki').innerHTML = (Obj * 0.001).toFixed(4) + ' dm' + '3'.sup();
+        document.getElementById('naglowek').innerHTML = 'Objętość:';
     } else {
-            document.getElementById('wyniki').innerHTML = 'Wybierz obliczenie i wprowadź dane!';
-            document.getElementById('naglowek').innerHTML = '';
+        document.getElementById('wyniki').innerHTML = 'Wybierz obliczenie i wprowadź dane!';
+        document.getElementById('naglowek').innerHTML = '';
     };
 
     return wynik;
