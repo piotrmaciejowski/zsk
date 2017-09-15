051ef02cbb7c6b04c569148ecc7e9383756191b0
piotrmaciejowski <maciejowski.piotrek@gmail.com>
2017-09-15 11:58:23 +0200

    Math c.d.

--- Math/script.js
+++ Math/script.js
@@ -36,3 +36,42 @@ var zaokraglenieB = Math.round(b);
 
 console.log(zaokraglenieA);
 console.log(zaokraglenieB);
+
+var zaokragleniemin = Math.round(minimalna);
+console.log(zaokragleniemin);
+
+//abs - wartość bezwzględna, round, floor
+
+var x = -56;
+var y = -13.1;
+var z = 14.9;
+
+document.write('<br>' + Math.abs(x)); //56
+document.write('<br>' + Math.abs(y)); //13.1
+document.write('<br>' + Math.abs(z)); //14.9
+document.write('<br>');
+document.write('<br>' + Math.round(x)); //-56
+document.write('<br>' + Math.round(y)); //-13
+document.write('<br>' + Math.round(z)); //15
+document.write('<br>');
+document.write('<br>' + Math.floor(x)); //-56
+document.write('<br>' + Math.floor(y)); //-13
+document.write('<br>' + Math.floor(z)); //15
+
+//potęgowanie
+
+var p = Math.pow(2,10);
+console.log(p); //1024
+
+//random
+
+for (var i = 0; i < 2; i ++){
+	var losuj = Math.random();
+	console.log(losuj);
+}
+
+//losuj z przedziału (0,10>
+
+var los = Math.floor(Math.random() * 11);
+console.log(los);
+
