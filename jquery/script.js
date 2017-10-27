f060d54d4e9de424bff55e29f76ee69564aeaf39
piotrmaciejowski <maciejowski.piotrek@gmail.com>
2017-10-27 08:52:25 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Obiekty/script.js

---
+++ jquery/script.js
@@ -0,0 +1,11 @@
+/*$(document).ready(function(){
+	$('#text').click(function(){
+		$('#lorem').toggle('slow');
+		//$('#lorem').toggle('fast');
+		//$('#lorem').toggle(3000);
+	})
+});*/
+
+$(document).ready(function(){
+
+});
