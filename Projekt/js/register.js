7e1881bf18732685bcfc75b3af17e1b55d07eec2
piotrmaciejowski <maciejowski.piotrek@gmail.com>
2017-10-30 06:54:11 +0100

    projekt

---
+++ Projekt/js/register.js
@@ -0,0 +1,362 @@
+$(document).ready (function (){
+
+var regImie = /^[a-ząśćźżółęń]{2,20}$/i;
+var regNazwisko = /^[a-ząśćźżółęń]{2,20}(\-[a-ząśćźżółęń]{2,10})?$/i;
+var regNazwiskoMyslnik = /[-]/;
+var regLogin = /^[a-z0-9]{1}[\w\.\-]{1,20}[a-z0-9]{1}$/i;
+var regMail = /^[a-z][\w\.\-\_]{0,30}@(\w{1,20}\.){1,3}[a-z]{1,3}$/;
+var regPass = /^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W\_]).{8,35})$/;
+
+//Imię
+
+function imieCheck(){
+    var imieValue = document.getElementById('input-imie').value;
+    var imieSpr = regImie.test(imieValue);
+    if (imieSpr){
+        $('#good-imie').css('display', 'inline');
+        $('#error-imie').css('display', 'none');
+    }
+    else {
+        $('#good-imie').css('display', 'none');
+        $('#info-imie').css('display', 'none');
+        $('#error-imie').css('display', 'inline');
+    }
+};
+
+$('#input-imie').focusin(function(){
+		$('#info-imie').css('visibility', 'visible');
+	});
+
+$('#input-imie').keyup(function(){
+    var imieValue = document.getElementById('input-imie').value;
+    if(imieValue != ''){
+        imieCheck();
+    };
+});
+
+$('#input-imie').blur(function(){
+    imieCheck();
+});
+
+$('#input-imie').focusout(function(){
+		$('#info-imie').css('visibility', 'hidden');
+	});
+
+$('#info-imie').hover(
+function(){
+    $('#help').css('background-color', '#4d9ffe');
+    $('#help h5').text('Imię musi składać się z od 2 do 20 liter.');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+$('#error-imie').hover(
+function(){
+    $('#help').css('background-color', '#e52222');
+    $('#help h5').text('Imię nie może zawierać cyfr oraz znaków specjalnych i musi składać się z od 2 do 20 liter!');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+$('#good-imie').hover(
+function(){
+    $('#help').css('background-color', '#1bc442');
+    $('#help h5').text('Imię poprawne!');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+//Nazwisko
+
+function nazwiskoCheck(){
+    var nazwiskoValue = document.getElementById('input-nazwisko').value;
+    var nazwiskoSpr = regNazwisko.test(nazwiskoValue);
+    if (nazwiskoSpr){
+        $('#good-nazwisko').css('display', 'inline');
+        $('#error-nazwisko').css('display', 'none');
+    }
+    else {
+        $('#good-nazwisko').css('display', 'none');
+        $('#info-nazwisko').css('display', 'none');
+        $('#error-nazwisko').css('display', 'inline');
+    }
+};
+
+$('#input-nazwisko').focusin(function(){
+		$('#info-nazwisko').css('visibility', 'visible');
+	});
+
+$('#input-nazwisko').keyup(function(){
+    var nazwiskoValue = document.getElementById('input-nazwisko').value;
+    if(nazwiskoValue != ''){
+        nazwiskoCheck();
+    };
+});
+
+$('#input-nazwisko').blur(function(){
+    nazwiskoCheck();
+});
+
+$('#input-nazwisko').focusout(function(){
+		$('#info-nazwisko').css('visibility', 'hidden');
+	});
+
+$('#info-nazwisko').hover(
+function(){
+    $('#help').css('background-color', '#4d9ffe');
+    $('#help h5').text('Nazwisko musi składać się z od 2 do 20 liter (30 w przypadku nazwisk dwuczłonowych).');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+$('#error-nazwisko').hover(
+function(){
+    $('#help').css('background-color', '#e52222');
+    $('#help h5').text('Nazwisko nie może zawierać cyfr oraz znaków specjalnych (innych niż myślnik) i musi składać się z od 2 do 20 liter (30 w przypadku nazwisk dwuczłonowych)!');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+$('#good-nazwisko').hover(
+function(){
+    $('#help').css('background-color', '#1bc442');
+    $('#help h5').text('Nazwisko poprawne!');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+//Login
+
+function loginCheck(){
+    var loginValue = document.getElementById('input-login').value;
+    var loginSpr = regLogin.test(loginValue);
+    if (loginSpr){
+        $('#good-login').css('display', 'inline');
+        $('#error-login').css('display', 'none');
+    }
+    else {
+        $('#good-login').css('display', 'none');
+        $('#info-login').css('display', 'none');
+        $('#error-login').css('display', 'inline');
+    }
+};
+
+$('#input-login').focusin(function(){
+		$('#info-login').css('visibility', 'visible');
+	});
+
+$('#input-login').keyup(function(){
+    var loginValue = document.getElementById('input-login').value;
+    if(loginValue != ''){
+        loginCheck();
+    };
+});
+
+$('#input-login').blur(function(){
+    loginCheck();
+});
+
+$('#input-login').focusout(function(){
+		$('#info-login').css('visibility', 'hidden');
+	});
+
+$('#info-login').hover(
+function(){
+    $('#help').css('background-color', '#4d9ffe');
+    $('#help h5').text('Login musi składać się z od 2 do 21 znaków. Nie może zaczynać ani kończyć się znakiem specjalnym.');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+$('#error-login').hover(
+function(){
+    $('#help').css('background-color', '#e52222');
+    $('#help h5').text('Login nie może zaczynać ani kończyć się znakiem specjalnym i musi składać się z od 2 do 21 znaków!');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+$('#good-login').hover(
+function(){
+    $('#help').css('background-color', '#1bc442');
+    $('#help h5').text('Login poprawny!');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+//Hasło
+
+function hasloCheck(){
+    var hasloValue = document.getElementById('input-haslo').value;
+    var hasloSpr = regPass.test(hasloValue);
+    var hasloPowtorz = document.getElementById('input-powtorz');
+    if (hasloSpr){
+        $('#good-haslo').css('display', 'inline');
+        $('#error-haslo').css('display', 'none');
+        hasloPowtorz.disabled = false;
+    }
+    else {
+        $('#good-haslo').css('display', 'none');
+        $('#info-haslo').css('display', 'none');
+        $('#error-haslo').css('display', 'inline');
+        hasloPowtorz.disabled = true;
+    }
+};
+
+function powtorzCheck(){
+var hasloValue = document.getElementById('input-haslo').value;
+var powtorzValue = document.getElementById('input-powtorz').value;
+    if(hasloValue == powtorzValue){
+        $('#good-powtorz').css('display', 'inline');
+        $('#error-powtorz').css('display', 'none');
+    }
+    else {
+        $('#good-powtorz').css('display', 'none');
+        $('#info-powtorz').css('display', 'none');
+        $('#error-powtorz').css('display', 'inline');
+    }
+}
+
+$('#input-haslo').focusin(function(){
+		$('#info-haslo').css('visibility', 'visible');
+	});
+
+$('#input-haslo').keyup(function(){
+    var hasloValue = document.getElementById('input-haslo').value;
+    if(hasloValue != ''){
+        hasloCheck();
+    };
+});
+
+$('#input-haslo').blur(function(){
+    hasloCheck();
+});
+
+$('#input-haslo').focusout(function(){
+		$('#info-imie').css('visibility', 'hidden');
+});
+
+$('#input-powtorz').focusin(function(){
+		$('#info-powtorz').css('visibility', 'visible');
+});
+
+$('#input-powtorz').keyup(function(){
+    var powtorzValue = document.getElementById('input-powtorz').value;
+    if(powtorzValue != ''){
+        powtorzCheck();
+    };
+});
+
+$('#input-powtorz').blur(function(){
+    powtorzCheck();
+});
+
+$('#input-powtorz').focusout(function(){
+		$('#info-powtorz').css('visibility', 'hidden');
+});
+
+$('#info-haslo').hover(
+function(){
+    $('#help').css('background-color', '#4d9ffe');
+    $('#help h5').text('Hasło musi składać się z od 8 do 32 znaków, w tym z przynajmniej jednej wielkiej litery, jednej cyfry oraz jednego znaku specjalnego.');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+$('#error-haslo').hover(
+function(){
+    $('#help').css('background-color', '#e52222');
+    $('#help h5').text('Hasło musi składać się z od 8 do 32 znaków, w tym z przynajmniej jednej wielkiej litery, jednej cyfry oraz jednego znaku specjalnego!');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+$('#good-haslo').hover(
+function(){
+    $('#help').css('background-color', '#1bc442');
+    $('#help h5').text('Hasło poprawne!');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+$('#info-powtorz').hover(
+function(){
+    $('#help').css('background-color', '#4d9ffe');
+    $('#help h5').text('Powtórz hasło.');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+$('#error-powtorz').hover(
+function(){
+    $('#help').css('background-color', '#e52222');
+    $('#help h5').text('Hasła nie są identyczne!');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+$('#good-powtorz').hover(
+function(){
+    $('#help').css('background-color', '#1bc442');
+    $('#help h5').text('Hasła są identyczne!');
+    $('#help').show(1000);
+},
+function(){
+    $('#help').hide(500);
+    $('#help h5').text('');
+});
+
+//E-mail
+
+$('#input-email').focusin(function(){
+		$('#info-email').css('visibility', 'visible');
+	});
+
+$('#input-email').focusout(function(){
+		$('#info-email').css('visibility', 'hidden');
+	});
+
+
+});
